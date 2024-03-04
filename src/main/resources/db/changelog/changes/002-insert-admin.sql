7b3e5842bc5dd25c0470e5de7fe3723b93570f4b
Imran T. Rasulzada <ITRasulzada@bankofbaku.com>
2024-03-04 11:54:05 +0400

    liquibase-changes added. admin and authoriy configured

---
+++ src/main/resources/db/changelog/changes/002-insert-admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO client (username, password, name, surname, email, phone, birthdate, status)
+VALUES ('admin', '$2a$10$oGwJUd4DAXtwhYuGmH0rK.TvCVp6AlPzpEKPxqU7WRwsqbC59ELXW', 'Admin', 'AdminSurname', 'admin@example.com', '+994505005050', '2000-01-01', 1);
